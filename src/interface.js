626c1973d286dd9332ba6cb0005e1c14d0f931d3
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-02-08 12:44:03 +0000

    slider input

--- src/interface.js
+++ src/interface.js
@@ -18,70 +18,96 @@ function populateWeather() {
   place = $('user-loc').value;
   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&appid=68d94d5bb085aaebdda7654eb41e9176`)
 
-  .then(function(response) {
-    return(response.json());
-  })
-  .then(function(myJson) {
-    data = myJson;
+    .then(function(response) {
+      return (response.json());
+    })
+    .then(function(myJson) {
+      data = myJson;
 
-    $('city').textContent = data.name
-    $('curtemp').textContent = `${Math.round(data.main.temp)}°C`
-    $('weathdescrip').textContent = data.weather[0].description
-    $('mintemp').textContent = `min: ${Math.round(data.main.temp_min)}°C`
-    $('maxtemp').textContent = `max: ${Math.round(data.main.temp_max)}°C`
-    $('humidity').textContent = `humidity: ${data.main.humidity}%`
-    $('wind').textContent = `wind: ${data.wind.speed} km/h`
-  }); 
+      $('user-loc').value = '';
+      $('btn-weather').style.visibility = 'hidden';
+
+      if (data.cod === '404') {
+        alert("Please enter a valid city.")
+      } else {
+        $('city').textContent = data.name
+        $('curtemp').textContent = `${Math.round(data.main.temp)}°C`
+        $('weathdescrip').textContent = data.weather[0].description
+        $('mintemp').textContent = `min: ${Math.round(data.main.temp_min)}°C`
+        $('maxtemp').textContent = `max: ${Math.round(data.main.temp_max)}°C`
+        $('humidity').textContent = `humidity: ${data.main.humidity}%`
+        $('wind').textContent = `wind: ${data.wind.speed} km/h`
+      }
+    });
+}
+
+function clearError() {
+  $('limit-errors').textContent = "";
+}
+
+function showSlider(){
+  $('myRange').style.visibility = 'visible';
+}
+
+function hideSlider() {
+  $('myRange').style.visibility = 'hidden';
+  setTimeout(showSlider, 1500)
 }
 
 currentStats();
 
-
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
   $('user-loc').value = 'London';
   populateWeather();
   $('user-loc').value = ''
 })
 
-
-$('user-loc').addEventListener('input', function success() {
-  if ($('user-loc').value.length < 3) { 
+$('user-loc').addEventListener('input', () => {
+  if ($('user-loc').value.length < 3) {
     $('btn-weather').disabled = true;
-    $('btn-weather').style.visibility = 'hidden';}
-  else { 
+    $('btn-weather').style.visibility = 'hidden';
+  } else {
     $('btn-weather').disabled = false;
     $('btn-weather').style.visibility = 'visible';
   }
 })
 
-
 $('btn-weather').addEventListener('click', () => {
   populateWeather();
 });
 
-$('btn-up').addEventListener('click', () => {
-  if (thermostat.temperature >= 25 && thermostat.powerSaving === true) {
-    alert("Disable Power Saving Mode to increase temperature.")
-  } else if (thermostat.temperature >= 32 && thermostat.powerSaving === false) {
-    alert("That's hot enough. If you want to sweat, go to the gym.")
-  } else {
-    thermostat.tempUp();
-    currentStats();
-  }
-})
-
-
-$('btn-down').addEventListener('click', () => {
+$('myRange').addEventListener('input', () => {
+  let val = $('myRange').value
   if (thermostat.temperature <= 10) {
-    alert("That's cold enough. Wouldn't want you to catch a cold.")
+    hideSlider()
+    $('limit-errors').textContent = "Minimum Temperature."
+    currentStats();
+    $('myRange').min = '10';
+    thermostat.changeTemp(val)
+    setTimeout(clearError, 1500)
+  } else if (thermostat.temperature >= 25 && thermostat.powerSaving === true) {
+    hideSlider()
+    $('limit-errors').textContent = "Disable Power Saving Mode to increase temperature."
+    currentStats();
+    $('myRange').max = '25';
+    thermostat.changeTemp(val)
+    setTimeout(clearError, 1500)
+  } else if (thermostat.temperature >= 32 && thermostat.powerSaving === false) {
+    hideSlider()
+    $('limit-errors').textContent = "Maximum Temperature."
+    currentStats();
+    $('myRange').max = '32';
+    thermostat.changeTemp(val)
+    setTimeout(clearError, 1500)
   } else {
-    thermostat.tempDown();
+    thermostat.changeTemp(val)
     currentStats();
   }
 })
 
 $('temp-reset').addEventListener('click', () => {
   thermostat.reset();
+  $('myRange').value = 20;
   currentStats();
 })
 
@@ -90,8 +116,11 @@ $('pwrsv').addEventListener('click', () => {
 
   if ($('pwrsv').className == 'pwrsvon') {
     $('pwrsv').className = 'pwrsvoff'
+    $('myRange').max = '32';
   } else {
     $('pwrsv').className = 'pwrsvon'
+    thermostat.temperature = 25;
+    $('myRange').value = 25
     currentStats();
   }
   $('pwrsv').textContent = (thermostat.getMode());
--- src/thermostat.js
+++ src/thermostat.js
@@ -28,24 +28,8 @@ class Thermostat {
 		}
 	}
 
-	tempUp() {
-		if (this.powerSaving == true && this.temperature === 25){
-			throw new Error("Max Temp On Power Saving Mode = 25 Degrees")
-		}
-		else if (this.powerSaving == false && this.temperature === 32){
-			throw new Error("Max Temp = 32 Degrees")
-		}
-		else{
-		this.temperature  += 1 
-		}
-	}
-
-	tempDown(){
-		if (this.temperature > 10){
-		this.temperature -= 1}
-		else {
-			throw new Error("10 degrees is frosty enough, bruh!")
-		}
+	changeTemp(val) {
+		this.temperature = val
 	}
 
 	reset() {
